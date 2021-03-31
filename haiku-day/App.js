f2ff55bdf5f2832c69e6bd554308c311bca73258
Daphaz <bonnet.damien@ymail.com>
2021-03-31 19:05:27 +0200

    feat(onboarding): add onboarding screen
    use a context for change the navigation screen
    Closes #1

--- haiku-day/App.js
+++ haiku-day/App.js
@@ -2,19 +2,22 @@ import React, { useState } from 'react'
 import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
 import { NavigationContainer } from '@react-navigation/native'
 import { DefaultTheme, DarkTheme } from './src/theme'
-import { ThemeContext } from './src/helpers/context'
-import TabsNavigation from './src/Tabs'
+import { ThemeContext, OnboardContext } from './src/helpers/context'
+import Navigation from './Navigation'
 
 export default function App() {
   const colorScheme = useColorScheme()
   const [theme, setTheme] = useState(colorScheme)
+  const [showOnboard, setShowOnboard] = useState(true)
 
   return (
     <AppearanceProvider>
       <ThemeContext.Provider value={{ theme, setTheme }}>
-        <NavigationContainer theme={theme === 'light' ? DefaultTheme : DarkTheme}>
-          <TabsNavigation />
-        </NavigationContainer>
+        <OnboardContext.Provider value={{ showOnboard, setShowOnboard }}>
+          <NavigationContainer theme={theme === 'light' ? DefaultTheme : DarkTheme}>
+            <Navigation />
+          </NavigationContainer>
+        </OnboardContext.Provider>
       </ThemeContext.Provider>
     </AppearanceProvider>
   )
