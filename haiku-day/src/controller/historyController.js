0626c7f9dd23323f9280842f2731a63ed8174525
Daphaz <bonnet.damien@ymail.com>
2021-04-08 17:52:57 +0200

    feat(controllerHistory): add query select all

--- haiku-day/src/controller/historyController.js
+++ haiku-day/src/controller/historyController.js
@@ -2,6 +2,37 @@ import { openDatabase } from './db'
 
 const sqlite = openDatabase()
 
+export const selectHistoryAll = async () => {
+  const db = await sqlite
+  let datas
+  return new Promise((res, rej) => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          'SELECT * from history',
+          null,
+          (tx, result) => {
+            if (result.rows.length > 0) {
+              datas = { status: true, msg: result.rows._array }
+            } else {
+              datas = { status: false, msg: 'Une heure est survenue, veuillez recommencer' }
+            }
+            res(datas)
+          },
+          (error) => {
+            datas = { status: false, msg: error.message, func: 'selectHistoryAll' }
+            res(datas)
+          },
+        )
+      },
+      (err) => {
+        datas = { status: false, msg: err.message, func: 'selectHistoryAll' }
+        rej(datas)
+      },
+    )
+  })
+}
+
 export const addHistoryItem = async (item) => {
   const db = await sqlite
   let datas
@@ -9,7 +40,7 @@ export const addHistoryItem = async (item) => {
     db.transaction(
       (tx) => {
         tx.executeSql(
-          'INSERT into history(haiku_id,text,date,author) VALUES(?,?,?,?)',
+          'INSERT into history(haiku_id,text,date,author,schedule_date) VALUES(?,?,?,?,?)',
           item,
           (tx, result) => {
             if (result.rowsAffected > 0) {
@@ -20,13 +51,13 @@ export const addHistoryItem = async (item) => {
             res(datas)
           },
           (error) => {
-            datas = { status: false, msg: error.message, func: 'addHistoryItem' }
+            datas = { status: false, msg: error, func: 'addHistoryItemError' }
             res(datas)
           },
         )
       },
       (err) => {
-        datas = { status: false, msg: err.message, func: 'addHistoryItem' }
+        datas = { status: false, msg: err, func: 'addHistoryItem' }
         rej(datas)
       },
     )
@@ -40,7 +71,7 @@ export const removeHistoryItem = async () => {
     db.transaction(
       (tx) => {
         tx.executeSql(
-          'DELETE FROM history',
+          'DELETE from history',
           null,
           (tx, result) => {
             if (result.rowsAffected > 0) {
